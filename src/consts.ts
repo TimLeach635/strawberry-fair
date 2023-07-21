8dc924edbb926ca2a2495e4dd0eb3afaec801b0b
Tim Leach <timleach635@gmail.com>
2023-07-21 16:30:11 +0100

    Change location of header files

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Strawberry Fair";
+export const SITE_DESCRIPTION = "By the people of Cambridge. For the people of Cambridge.";
